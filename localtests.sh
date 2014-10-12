b090e698f37679c66d21c0ce7cbacca67a78da1e
Jason Livesay <ithkuil@gmail.com>
2014-10-11 18:45:36 -0700

    local setup

--- localtests.sh
+++ localtests.sh
@@ -7,7 +7,15 @@ set -e
 pgrep --help >/dev/null
 
 echo Making sure submodules are downloaded.
-if [ ! -d "$
+if [ ! -f "serious-backup-device/package.json" ]; then
+  echo "No serious-backup-device module found"
+  exit 1
+fi
+
+if [ ! -f "serious-backup-server/package.json" ]; then
+  echo "No serious-backup-server module found"
+  exit 1
+fi
 
 echo Running tests on local system with mocha.
 
--- test/localsetup.js
+++ test/localsetup.js
@@ -4,7 +4,7 @@ var should = require('should')
 checkRunning = function(match, cb) {
   console.log("Running pgrep to check if " + match + " is already running.");
   console.log("Note that this won't work if there is no pgrep (did " +
-              "you start the test using the localtests.sh script?);
+              "you start the test using the localtests.sh script?");
 
   childproc.exec('pgrep -fx ' + match + ' 2>&1', function(e,out,er) {
     if (e) {
@@ -18,7 +18,14 @@ checkRunning = function(match, cb) {
 }
 
 startDevice = function(cb) {
-
+  childproc.exec('cd ../serious-backup-device && npm start', function(e,out,stderr) {
+    e.should.not.be.ok;
+    console.log(out);
+    if (stderr) {
+      console.log(stderr);
+    }
+    cb();
+  });
 }
 
 startServer = function(cb) {
@@ -26,16 +33,13 @@ startServer = function(cb) {
 }
 
 beforeTests = function(done) {
-  checkRunning(
-
-  childproc.exec('cd ../serious-backup-device && npm start', function(e,out,stderr) {
-    e.should.not.be.ok;
-    console.log(out);
-    if (stderr) {
-      console.log(stderr);
+  checkRunning('apiserver.js', function(running) {
+    if (!running) {
+      startDevice(done);
+   } else {
+      done();
     }
   });
-
 }
 
 exports.beforeTests = beforeTests();
