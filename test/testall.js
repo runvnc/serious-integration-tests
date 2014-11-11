a11911b61518ba59e5720e627904b5fa339365bf
Jason Livesay <ithkuil@gmail.com>
2014-11-10 20:47:59 -0800

    deviceid

--- test/testall.js
+++ test/testall.js
@@ -2,7 +2,6 @@ var should = require('should')
   , sdk = require('../serious-backup-device-sdk');
 
 describe('backup complete integration', function() {
-  var backupKey = null;
   var backupTime = null;
 
   before(function(done) {
@@ -29,12 +28,6 @@ describe('backup complete integration', function() {
     it('should start the backup and return a backup time', function(done) {
       this.timeout(26000);
       sdk.backup(function(data) {
-        console.log('backup returns: ***********');
-        console.log(data);
-        var key = data.key;
-        key.should.be.ok;
-        key.length.should.be.greaterThan(4);
-        backupKey = key;
         data.time.should.be.ok;
         backupTime = data.time;
         // wait a second for backup to start.
@@ -43,29 +36,11 @@ describe('backup complete integration', function() {
     })
   })
 
-  describe('#generalStatus()', function() {
-    it('should indicate a backup is running', function(done) {
-      this.timeout(35000);
-      checkFinished = function() {
-        sdk.generalStatus(function(currStatus) {
-          currStatus.should.be.ok;
-          console.log("current status:");
-          console.log(currStatus);
-          if (currStatus.activeCount == 0) {
-            return done();
-          } else {
-            setTimeout(function() { checkFinished(); }, 25000);
-          }
-        });
-      }
-      checkFinished();
-    })
-  })
- 
+/*
   describe('#restore()', function(done) {
     it('should restore the backup', function(done) {
       this.timeout(45000); 
-      sdk.restore(backupKey, backupTime, function(ret) {
+      sdk.restore(backupTime, function(ret) {
         ret.should.be.ok;
         setTimeout(function() { done(); }, 36000);
       });
@@ -82,5 +57,6 @@ describe('backup complete integration', function() {
       });
     });
   });
+*/
  
 });
